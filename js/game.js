fb26bf0ba526b3e7a50a15de8fc6c5d63fc9aaa7
Xinyang <xinyzhang9@gmail.com>
2016-01-07 16:11:18 -0800

    finish beam feature

--- js/game.js
+++ js/game.js
@@ -1,11 +1,13 @@
-function PlayGround(selector_ch1)
+function PlayGround(selector_ch1,selector_beam1)
 {
 	//create the first character
 	var ch1 = new Character(selector_ch1);
+	var beam1 = new Beam(selector_beam1,ch1.ch_x);
 	
 	//attaches event listener to the document listening for key strokes
 	this.initialize = function()
 	{
+		document.getElementById('beam1').style.visibility = "hidden";
 		$(document).keydown(function(e) {
 			//if the user pressed 'D'
 			if(e.keyCode == 39) {
@@ -25,6 +27,10 @@ function PlayGround(selector_ch1)
 			}
 			else if(e.keyCode == 68) {
 				ch1.updateAction("BEAM");
+				beam1.ch_x = ch1.ch_x;
+				beam1.old_x = ch1.ch_x;
+				document.getElementById('beam1').style.visibility = "visible";
+
 			}
 			else if(e.keyCode == 70) {
 				ch1.updateAction("ROUND_HOUSE");
@@ -37,6 +43,7 @@ function PlayGround(selector_ch1)
 	this.mainLoop = function()
 	{
 		ch1.drawCharacter();
+		beam1.drawCharacter();
 	}
 
 
@@ -75,6 +82,7 @@ function Character(selector)
 		counter=0;
 		y_counter = 0;
 		this.action = action;
+
 	}
 	//updates the character's coordinates and changes the sprite's counter to simulate the character moving
 	this.updateCoordinate = function()
@@ -111,3 +119,61 @@ function Character(selector)
 	}
 }
 
+function Beam(selector,pos)
+{
+	var selector = selector; //store the html id of the BEAM
+
+	var constants = {
+		'START': 		{ 'y': 5, 'x': [0, 1] },
+		'END':  		{ 'y': 5, 'x': [2, 3] },
+		'MOVE': 	{ 'y': 4, 'x': [0, 1] },
+
+	}
+	var counter = 0;			//stores which sprite (in the x-direction) it should display 
+	this.action = "MOVE";	//default action is for the character to stand
+	this.ch_x=pos;					//x_coordinate of the character
+	this.ch_y=120;					//y_coordinate of the character
+	this.old_x = pos;	//start position of beam as a refence
+	//ch_x, ch_y and action could really all be private variables and I could have just done var instead of this. but to make debuggin easier, I am making them an instance variable so that it would display when you log the chracter object
+
+	this.drawSprite = function(y, x)
+	{
+		$('#'+selector).css('background', "url('images/ken.png') "+x*(-70)+"px "+(-80*y)+"px").css('left', this.ch_x+"px").css('top',this.ch_y+"px");
+	}
+
+	//updates the action
+	this.updateAction = function(action)
+	{
+		counter=0;
+		this.action = action;
+	}
+	//updates the character's coordinates and changes the sprite's counter to simulate the character moving
+	this.updateCoordinate = function()
+	{
+		if(counter>=constants[this.action].x.length)
+		{
+			counter=0;
+			//if action is anything other than 'STANDING' change the action back to 'STANDING'
+			this.action = 'MOVE';
+		}
+
+		this.ch_x +=20;
+		if(this.ch_x - this.old_x <=20){
+			this.action = 'START';
+		}
+		else if(this.ch_x - this.old_x >= 200){
+			this.action = 'END';
+		}else{
+			this.action = 'MOVE';
+		}
+
+	}
+
+	//draws the character on the screen
+	this.drawCharacter = function()
+	{
+		// console.log("drawing character");
+		this.updateCoordinate();
+		this.drawSprite(constants[this.action].y, constants[this.action].x[counter++]);
+	}
+}
